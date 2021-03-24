7d6f90074300ac5ce152b03366dd0c065cebb15c
shinequasar <shinequasar@gmail.com>
2021-03-24 23:19:31 +0900

    [Upload] first upload

---
+++ read-csv.py
@@ -0,0 +1,6 @@
+import pandas as pd
+#컬럼명 안쓰고 컬럼이름을 value로 두고싶을때
+#data = pd.read_csv("eggs.csv",header=None, encoding='CP949') 
+
+#컬럼명 쓰고싶을때
+data = pd.read_csv("eggs.csv",header=None, encoding='CP949')
